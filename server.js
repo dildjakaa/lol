228a7315a84ec3d7f14b78dc2179460b524096ad
dildjakaa <genii1298@gmail.com>
2025-09-07 14:38:11 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const WebSocket = require('ws');
 const { exec } = require('child_process');
-const https = require('https');
+const server = http.createServer(app);
 const fs = require('fs');
 
 const app = express();
